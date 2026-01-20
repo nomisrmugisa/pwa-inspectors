731e4c82a6fb0580ab480cf0fc4d2bdc95423ddf
Simon Kununka <skununka@hispuganda.org>
2026-01-20 19:43:12 +0300

    updto date with Hospital

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-95dee3cf'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dtqqal5qv88"
+    "revision": "0.acm4jou44ho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
